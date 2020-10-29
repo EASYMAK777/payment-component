c316986e7780006e52da5cfe2754fe0e2a751ee3
EASYMAK777 <jplocallawncare@gmail.com>
2020-10-28 20:27:52 -0400

    useRef and UseEffect to render the paypal button

--- src/components/Paypal.js
+++ src/components/Paypal.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function Paypal() {
+    
+    const paypal = useRef()
+
+    useEffect(() => {
+        window.paypal.Buttons({
+            
+        }) 
+    }, [])
+    
     return(
         <div>
-            
+            <div ref={paypal}></div>
         </div>
     )
 }
