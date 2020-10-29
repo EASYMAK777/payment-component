cc1402601a7933936d5c243fa8ca9c13b2697871
EASYMAK777 <jplocallawncare@gmail.com>
2020-10-29 04:20:17 -0400

    paypal checkout buttion functional, but paypal having log in issues, will fully test tomorrow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { useState } from 'react';
+import Paypal from './components/Paypal'
 
 function App() {
 
--- src/components/Paypal.js
+++ src/components/Paypal.js
@@ -5,10 +5,35 @@ export default function Paypal() {
     const paypal = useRef()
 
     useEffect(() => {
-        window.paypal.Buttons({
+        window.paypal
+        .Buttons({
+            createOrder: (data, actions, err) => {
+                return actions.order.create({
+                    intent: "CAPTURE",
+                    purchase_units: [
+                        {
+                            description: "Video Game",
+                            amount: {
+                                currency_code: "USD",
+                                value: 1000,
+
+                            },
+                        },
+                    ],
+                });
+                
+            },
+            onApprove: async (data, actions) => {
+                const order = await actions.order.capture();
+                console.log(order);
+            },
+            onError: (err) => {
+                console.log(err)
+            }
             
-        }).render(paypal.current) 
-    }, [])
+        })
+        .render(paypal.current) 
+    }, []);
     
     return(
         <div>
